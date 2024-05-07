88914bafff84c8865db641b2411890b57faf29ce
Josh Massa <joshmassa@gmail.com>
2024-05-07 19:12:05 -0400

    Adjusted Navigation.jsx/css, added Header.jsx

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -3,24 +3,24 @@ import { NavLink } from 'react-router-dom';
 import '../styles/Navigation.css';
 
 function Navigation() {
-    const [isOpen, setIsOpen] = useState(false);
-    const currentPath = window.location.pathname;
+  const [isOpen, setIsOpen] = useState(false);
+  const getNavLinkClass = ({ isActive }) => isActive ? "links active-link" : "links";
 
   return (
-    <nav className="navbar">
-        <NavLink id='name' className="links" to="/" activeClassName="active-link">Joshua Massa Pelletier</NavLink>
-        <button className={`menu-toggle ${isOpen ? 'active' : ''}`} onClick={() => setIsOpen(!isOpen)}>
-            <span className="bar"></span>
-            <span className="bar"></span>
-            <span className="bar"></span>
-        </button>
-        <div className={`nav-links ${isOpen ? 'active' : ''}`}>
-            <NavLink className="links" to="/about" activeClassName="active-link">About Me</NavLink>
-            <NavLink className="links" to="/portfolio" activeClassName="active-link">Portfolio</NavLink>
-            <NavLink className="links" to="/contact" activeClassName="active-link">Contact</NavLink>
-            <NavLink className="links" to="/resume" activeClassName="active-link">Resume</NavLink>
-        </div>
-    </nav>
+      <nav className="navbar">
+          <NavLink to="/" className={`${getNavLinkClass} name`}>Joshua Massa Pelletier</NavLink>
+          <button className={`menu-toggle ${isOpen ? 'active' : ''}`} onClick={() => setIsOpen(! isOpen)}>
+              <span className="bar"></span>
+              <span className="bar"></span>
+              <span className="bar"></span>
+          </button>
+          <div className={`nav-links ${isOpen ? 'active' : ''}`}>
+              <NavLink to="/about" className={getNavLinkClass}>About Me</NavLink>
+              <NavLink to="/portfolio" className={getNavLinkClass}>Portfolio</NavLink>
+              <NavLink to="/contact" className={getNavLinkClass}>Contact</NavLink>
+              <NavLink to="/resume" className={getNavLinkClass}>Resume</NavLink>
+          </div>
+      </nav>
   );
 }
 
