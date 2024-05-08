7bf8dcea41e21ed44dbb8cafe19ecc894b6942b6
Josh Massa <joshmassa@gmail.com>
2024-05-08 13:27:23 -0400

    Added Home.jsx/css placeholder

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,22 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import About from "./components/About";
-import Header from "./components/Header";
-import Footer from "./components/Footer";
+import About from './components/About';
+import Header from './components/Header';
+import Home from './components/Home';
+import Footer from './components/Footer';
 
 function App() {
 
   return (
     <Router>
-    <>
-      <Header />
-      {/* <Routes>
-        <Route path="/" exact component={HomePage} />
-        <Route path="/about" component={AboutPage} />
-        <Route path="/portfolio" component={PortfolioPage} />
-        <Route path="/contact" component={ContactPage} />
-        <Route path="/resume" component={ResumePage} />
-      </Routes> */}
-      <About />
-      <Footer />
-    </>
+      <>
+        <Header />
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/about' element={<About />} />
+        </Routes>
+        <Footer />
+      </>
     </Router>
   )
 }
