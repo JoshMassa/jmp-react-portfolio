a6e04c073d2ad54b0810a4e4e56122aff62fe2de
Josh Massa <joshmassa@gmail.com>
2024-05-11 20:03:42 -0400

    Modified image linking in attempt to fix broken images on deployed Netlify app

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,19 +1,22 @@
 import '../styles/Footer.css';
+import githubLogo from '../assets/github-mark-white.png';
+import linkedinLogo from '../assets/linkedin-logo.png';
+import twitterLogo from '../assets/twitter-icon.png';
 
 function Footer() {
 
     return (
         <footer>
             <a href='https://github.com/JoshMassa' target='_blank'>
-                <img id='github-logo' src='../src/assets/github-mark-white.png' alt='Github Logo'></img>
+                <img id='github-logo' src={githubLogo} alt='Github Logo'></img>
             </a>
 
             <a href='https://www.linkedin.com/in/joshua-massa-pelletier/' target='_blank'>
-                <img id='linkedin-logo' src='../src/assets/linkedin-logo.png' alt='LinkedIn Logo'></img>
+                <img id='linkedin-logo' src={linkedinLogo} alt='LinkedIn Logo'></img>
             </a>
 
             <a href='https://twitter.com/JoshMassa22' target='_blank'>
-                <img id='twitter-logo' src='../src/assets/twitter-icon.png' alt='Twitter Logo'></img>
+                <img id='twitter-logo' src={twitterLogo} alt='Twitter Logo'></img>
             </a>
         </footer>
     )
--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -1,6 +1,12 @@
 import React from 'react';
 import Project from '../components/Project';
 import '../styles/Portfolio.css';
+import melodyExchangeImage from '../assets/melody-exchange-ss.png';
+import employeeTrackerGif from '../assets/employee-tracker.gif';
+import socialMediaApiGif from '../assets/social-media-api.gif';
+import smileGeneratorGif from '../assets/smile-generator.gif';
+import noteTakerImage from '../assets/note-taker.png';
+import techBlogImage from '../assets/tech-blog-ss.png';
 
 function Portfolio() {
     const instructions = 'Click the title or Github logo for more information.';
@@ -12,7 +18,7 @@ function Portfolio() {
             title: 'Melody Exchange',
             description: 'A Full Stack Application where users can buy and sell used instruments.',
             instructions,
-            imageUrl: '../src/assets/melody-exchange-ss.png',
+            imageUrl: melodyExchangeImage,
             projectUrl: 'https://melody-exchange-206a7ddfc868.herokuapp.com/',
             githubUrl: 'https://github.com/plucafo/melody-exchange'
         },
@@ -21,7 +27,7 @@ function Portfolio() {
             title: 'Employee Tracker',
             description: 'A command-line application that accepts user input to view and manage departments, roles, and employees of a company.',
             instructions,
-            imageUrl: '../src/assets/employee-tracker.gif',
+            imageUrl: employeeTrackerGif,
             projectUrl: 'https://drive.google.com/file/d/1ocnF2lDZoVTmjFu9cXO_WQr09ug0SO8b/view',
             githubUrl: 'https://github.com/JoshMassa/employee-tracker'
         },
@@ -30,7 +36,7 @@ function Portfolio() {
             title: 'Social Media API',
             description: 'A Social Media API designed to manage users, their posts, and reactions to these posts.',
             instructions,
-            imageUrl: '../src/assets/social-media-api.gif',
+            imageUrl: socialMediaApiGif,
             projectUrl: 'https://drive.google.com/file/d/1ud2H61gVAObP5pFZSt5cuARcnAVptl0k/view',
             githubUrl: 'https://github.com/JoshMassa/social-media-api'
         },
@@ -39,7 +45,7 @@ function Portfolio() {
             title: 'Smile Generator',
             description: 'An application that gives users the ability to view randomly generated jokes and pictures to brighten their day.',
             instructions,
-            imageUrl: '../src/assets/smile-generator.gif',
+            imageUrl: smileGeneratorGif,
             projectUrl: 'https://joshmassa.github.io/smile-generator/',
             githubUrl: 'https://github.com/JoshMassa/smile-generator'
         },
@@ -48,7 +54,7 @@ function Portfolio() {
             title: 'Note Taker',
             description: 'An application that allows users to record and save notes so they can organize their thoughts and keep track of tasks they need to complete.',
             instructions,
-            imageUrl: '../src/assets/note-taker.png',
+            imageUrl: noteTakerImage,
             projectUrl: 'https://note-taker-jmp-5dc91171bb01.herokuapp.com/',
             githubUrl: 'https://github.com/JoshMassa/note-taker'
         },
@@ -57,7 +63,7 @@ function Portfolio() {
             title: 'Tech Blog',
             description: 'A full-stack blog application that allows users to view posts and comments. If a user signs up, they also have the ability to add, edit, and delete their own posts.',
             instructions,
-            imageUrl: '../src/assets/tech-blog-ss.png',
+            imageUrl: techBlogImage,
             projectUrl: 'https://jm-tech-blog-fe718f2a2321.herokuapp.com/',
             githubUrl: 'https://github.com/JoshMassa/tech-blog'
         }
