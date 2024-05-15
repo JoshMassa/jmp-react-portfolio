a0ebff86e48c4703442038bc6b30af0371d7e442
Josh Massa <joshmassa@gmail.com>
2024-05-15 19:13:53 -0400

    added game content/logic to easteregg page

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import About from './pages/About';
 import Contact from './pages/Contact';
+import EasterEgg from './pages/EasterEgg';
 import Footer from './components/Footer';
 import Header from './components/Header';
-import Home from './pages/Home';
 import Portfolio from './pages/Portfolio';
 import Resume from './pages/Resume';
 
@@ -18,7 +18,7 @@ function App() {
           <Routes>
             <Route path='/' element={<About />} />
             <Route path='/contact' element={<Contact />} />
-            <Route path='/easteregg' element={<Home />} />
+            <Route path='/easteregg' element={<EasterEgg />} />
             <Route path='/portfolio' element={<Portfolio />} />
             <Route path='/resume' element={<Resume />} />
           </Routes>
---
+++ src/pages/EasterEgg.jsx
@@ -0,0 +1,238 @@
+import React from 'react';
+import '../styles/EasterEgg.css';
+import backgroundImage from '../assets/background-single.png';
+import playerImage from '../assets/player.png';
+import enemyImage from '../assets/enemy-1.png';
+
+window.addEventListener('load', function() {
+    const canvas = document.getElementById('canvas1');
+    const ctx = canvas.getContext('2d');
+    canvas.width = 1000;
+    canvas.height = 720;
+    let enemies = [];
+    let score = 0;
+    let gameOver = false;
+
+    class InputHandler {
+        constructor() {
+            this.keys = [];
+            window.addEventListener('keydown', e => {
+                if ((   e.key === 'ArrowDown' ||
+                        e.key === 'ArrowUp' ||
+                        e.key === 'ArrowLeft' ||
+                        e.key === 'ArrowRight')
+                        && this.keys.indexOf(e.key) === -1) {
+                    this.keys.push(e.key);
+                }
+            });
+            window.addEventListener('keyup', e => {
+                if (    e.key === 'ArrowDown' ||
+                        e.key === 'ArrowUp' ||
+                        e.key === 'ArrowLeft' ||
+                        e.key === 'ArrowRight') {
+                    this.keys.splice(this.keys.indexOf(e.key), 1);
+                }
+            });
+        }
+    }
+
+    class Player {
+        constructor(gameWidth, gameHeight) {
+            this.gameWidth = gameWidth;
+            this.gameHeight = gameHeight;
+            this.width = 200;
+            this.height = 200;
+            this.x = 0;
+            this.y = this.gameHeight - this.height;
+            this.image = document.getElementById('playerImage');
+            this.frameX = 0;
+            this.maxFrame = 8;
+            this.frameY = 0;
+            this.fps = 20;
+            this.frameTimer = 0;
+            this.frameInterval = 1000/this.fps;
+            this.speed = 0;
+            this.vy = 0;
+            this.weight = 0.1;
+        }
+        draw(context) {
+            context.drawImage(this.image, this.frameX * this.width, this.frameY * this.height, this.width, this.height, this.x, this.y, this.width, this.height);
+        }
+        update(input, deltaTime, enemies) {
+            // Collision Detection
+            enemies.forEach(enemy => {
+                const dx = (enemy.x + enemy.width / 2) - (this.x + this.width / 2);
+                const dy = (enemy.y + enemy.height /2) - (this.y + this.height / 2);
+                const distance = Math.sqrt(dx * dx + dy * dy)
+                if (distance < enemy.width / 2 + this.width / 2) {
+                    gameOver = true;
+                }
+            })
+            // Sprite Animation
+            if (this.frameTimer > this.frameInterval) {
+                if (this.frameX >= this.maxFrame) this.frameX = 0;
+                else this.frameX++;
+                this.frameTimer = 0;
+            } else {
+                this.frameTimer += deltaTime;
+            }
+            // Controls
+            if (input.keys.indexOf('ArrowRight') > -1) {
+                this.speed = 2;
+            } else if (input.keys.indexOf('ArrowLeft') > -1) {
+                this.speed = -2;
+            } else if (input.keys.indexOf('ArrowUp') > -1 && this.onGround()) {
+                this.vy -= 10;
+            } else {
+                this.speed = 0;
+            }
+            // Horizontal Movement
+            this.x += this.speed;
+            if (this.x < 0) this.x = 0;
+            else if (this.x > this.gameWidth - this.width) this.x = this.gameWidth - this.width;
+            // Vertical Movement
+            this.y += this.vy;
+            if (!this.onGround()) {
+                this.vy += this.weight;
+                this.maxFrame = 5;
+                this.frameY = 1;
+            } else {
+                this.vy = 0;
+                this.maxFrame = 8;
+                this.frameY = 0;
+            }
+            if (this.y > this.gameHeight - this.height) this.y = this.gameHeight - this.height;
+        }
+        onGround() {
+            return this.y >= this.gameHeight - this.height;
+        }
+    }
+
+    class Background {
+        constructor(gameWidth, gameHeight) {
+            this.gameWidth = gameWidth;
+            this.gameHeight = gameHeight;
+            this.image = document.getElementById('backgroundImage');
+            this.x = 0;
+            this.y = 0;
+            this.width = 2400;
+            this.height = 720;
+            this.speed = 0.7;
+        }
+        draw(context) {
+            context.drawImage(this.image, this.x, this.y, this.width, this.height);
+            context.drawImage(this.image, this.x + this.width -3, this.y, this.width, this.height);
+        }
+        update() {
+           this.x -= this.speed;
+           if (this.x < 0 - this.width) this.x = 0;
+        }
+    }
+
+    class Enemy {
+        constructor(gameWidth, gameHeight) {
+            this.gameWidth = gameWidth;
+            this.gameHeight = gameHeight;
+            this.width = 160;
+            this.height = 119;
+            this.image = document.getElementById('enemyImage');
+            this.x = this.gameWidth;
+            this.y = this.gameHeight - this.height;
+            this.frameX = 0;
+            this.maxFrame = 5;
+            this.fps = 20;
+            this.frameTimer = 0;
+            this.frameInterval = 1000/this.fps;
+            this.speed = 2;
+            this.markedForDeletion = false;
+        }
+        draw(context) {
+            context.drawImage(this.image, this.frameX * this.width, 0, this.width, this.height, this.x, this.y, this.width, this.height);
+        }
+        update(deltaTime) {
+            if (this.frameTimer > this.frameInterval) {
+                if (this.frameX >= this.maxFrame) this.frameX = 0;
+                else this.frameX++;
+                this.frameTimer = 0;
+            } else {
+                this.frameTimer += deltaTime;
+            }
+            this.x -= this.speed;
+            if (this.x < 0 - this.width) {
+                this.markedForDeletion = true;
+                score++;
+            }
+        }
+    }
+
+    function handleEnemies(deltaTime) {
+        if (enemyTimer > enemyInterval + randomEnemyInterval) {
+            enemies.push(new Enemy(canvas.width, canvas.height));
+            console.log(enemies);
+            randomEnemyInterval = Math.random() * 1000 + 500;
+            enemyTimer = 0;
+        } else {
+            enemyTimer += deltaTime;
+        }
+        enemies.forEach(enemy => {
+            enemy.draw(ctx);
+            enemy.update(deltaTime);
+        });
+        enemies = enemies.filter(enemy => !enemy.markedForDeletion);
+    }
+
+    function displayStatusText(context) {
+        context.font = '35px Helvetica';
+        context.fillStyle = 'black';
+        context.fillText('Score: ' + score, 20, 50);
+        context.fillStyle = 'white';
+        context.fillText('Score: ' + score, 22, 52);
+        if (gameOver) {
+            context.textAlign = 'center';
+            context.fillStyle = 'black';
+            context.fillText('GAME OVER!', canvas.width / 2, 150);
+            context.fillStyle = 'black';
+            context.fillText('Don\'t touch the enemies!', canvas.width / 2, 200);
+            context.fillStyle = 'white';
+            context.fillText('GAME OVER!', canvas.width / 2 + 2, 152);
+            context.fillStyle = 'white';
+            context.fillText('Don\'t touch the enemies!', canvas.width / 2 + 2, 202);
+        }
+    }
+
+    const input = new InputHandler();
+    const player = new Player(canvas.width, canvas.height);
+    const background = new Background(canvas.width, canvas.height);
+
+    let lastTime = 0;
+    let enemyTimer = 0;
+    let enemyInterval = 1000;
+    let randomEnemyInterval = Math.random() * 1000 + 500;
+
+    function animate(timeStamp) {
+        const deltaTime = timeStamp - lastTime;
+        lastTime = timeStamp;
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        background.draw(ctx);
+        background.update();
+        player.draw(ctx);
+        player.update(input, deltaTime, enemies);
+        handleEnemies(deltaTime);
+        displayStatusText(ctx);
+        if (!gameOver) requestAnimationFrame(animate);
+    }
+    animate(0);
+});
+
+function EasterEgg() {
+    return (
+        <div className='easteregg-content'>
+            <canvas id='canvas1'></canvas>
+            <img src={playerImage} id='playerImage' alt='Player Image' />
+            <img src={backgroundImage} id='backgroundImage' alt='Background Image' />
+            <img src={enemyImage} id='enemyImage' alt='Enemy Image' />
+        </div>
+    )
+}
+
+export default EasterEgg;
